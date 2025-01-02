d4b43e27619c944056b74992c7a4892eb9719756
Uthman-Abbas <149466534+Uthman-Abbas@users.noreply.github.com>
2025-01-02 20:34:01 +0500

    Add files via upload

---
+++ CameraTest.py
@@ -0,0 +1,10 @@
+import cv2
+
+index = 0
+while True:
+    cap = cv2.VideoCapture(index)
+    if not cap.isOpened():
+        break
+    print(f"Camera {index} is available")
+    cap.release()
+    index += 1
